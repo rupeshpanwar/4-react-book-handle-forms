b3c8383da3617b5e4aa93f5767c9c0dbe487b15e
Jon Doe <jondoe@doe.com>
2023-02-15 10:43:31 +0800

    Initial commit

---
+++ src/Project/ProjectStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace Project
+{
+    public class ProjectStack : Stack
+    {
+        internal ProjectStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
