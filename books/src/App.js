e3c2a09109cfb76c4577d64b6ce3411bb8d5e9dd
Jon Doe <jondoe@doe.com>
2023-02-16 22:59:14 +0800

    step7- delete book from books array func

--- books/src/App.js
+++ books/src/App.js
@@ -7,6 +7,11 @@ function App() {
 
   const [books,setBooks] = useState([])
 
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => book.id !== id);
+    setBooks(updatedBooks);
+  };
+
   const createBook = (title) => {
     const updatedBooks = [...books,
        { id: books.length + 1, title }]
@@ -15,7 +20,7 @@ function App() {
 
   return (<div className='app'>  
     <BookCreate onCreate={createBook}/>
-    <BookList books={books} />
+    <BookList onDelete={deleteBookById} books={books} />
      </div>
   )
 }
--- books/src/Component/BookShow.js
+++ books/src/Component/BookShow.js
@@ -1,9 +1,17 @@
 
-function BookShow({book}){
+function BookShow({book, onDelete}){
 
-            return (
+    const handleDelete = () => {
+        onDelete(book.id);
+      };
+
+     return (
             <div className='book-show'>
             {book.title}
+           <div className="actions"> 
+                <button className="delete" onClick={handleDelete}>Delete
+                </button>
+           </div>
             </div>
             );
 };
