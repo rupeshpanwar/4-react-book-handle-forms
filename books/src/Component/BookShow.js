715759d55a6ec85302d12f41776a3b6f5c03db84
rupesh.panwar <rupeshpanwar28@gmail.com>
2023-02-17 11:31:19 +0800

    step8-add BookEdit functionality

--- books/src/Component/BookShow.js
+++ books/src/Component/BookShow.js
@@ -1,14 +1,30 @@
+import { useState } from "react";
+import BookEdit from './BookEdit'
 
 function BookShow({book, onDelete}){
+
+   const [showEdit,setShowEdit] = useState(false)
 
     const handleDelete = () => {
         onDelete(book.id);
       };
 
+      const handleEdit = () => {
+        setShowEdit(!showEdit)
+      }
+
+      let content = <h3>{book.title}</h3>
+
+      if(showEdit){
+        content = <BookEdit />
+      }
+
      return (
             <div className='book-show'>
-            {book.title}
+            <div>{content}</div>
+           
            <div className="actions"> 
+                <button className="edit" onClick={handleEdit} >Edit</button>
                 <button className="delete" onClick={handleDelete}>Delete
                 </button>
            </div>
